48d0db3f6b1278d25e5a1e4cba3e65a5e0fad2b3
ParveendThakur <parveend1234@gmail.com>
2024-04-13 01:05:59 +0530

    Variables Printing

---
+++ (Python) Variables Printing.py
@@ -0,0 +1,9 @@
+# Define a variable called number and initialize it with the value 10
+number = 10
+
+print("The value stored in the variable 'number' is:", number)
+
+# Assign the variable number with a new value, 20
+number = 20
+
+print("After assigning a new value, the value stored in the variable 'number' is:", number)
