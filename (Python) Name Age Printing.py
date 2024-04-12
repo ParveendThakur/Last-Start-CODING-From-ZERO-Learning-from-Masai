24b2949644ee27e12fd5d02469e8370e9f2100a2
ParveendThakur <parveend1234@gmail.com>
2024-04-13 01:16:50 +0530

    Name Age Printing

---
+++ (Python) Name Age Printing.py
@@ -0,0 +1,9 @@
+
+name = "ParveendThakur"
+
+age = 24
+
+
+# Print the values stored in the variables along with their types
+print("Name:", name, "| Type:", type(name))
+print("Age:", age, "| Type:", type(age))
