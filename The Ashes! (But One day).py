d0001e10ec3b4553bd2e78c4fd9666c2f60658ac
ParveendThakur <parveend1234@gmail.com>
2024-04-16 23:26:34 +0530

    panding Q

---
+++ The Ashes! (But One day).py
@@ -0,0 +1,9 @@
+def compare_countries(Australia, England):
+    
+    if Australia>England:
+        print ("Australia")
+    elif: Australia>England:
+        print("England")
+    else if: Australia==England
+        print("Tie")
+   
