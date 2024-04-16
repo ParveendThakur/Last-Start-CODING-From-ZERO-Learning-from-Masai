f218d019987d0f7374a5c5507c2a5a387a62c343
ParveendThakur <parveend1234@gmail.com>
2024-04-17 00:12:28 +0530

    DONE (Update & Compare)

--- Update & Compare.py
+++ Update & Compare.py
@@ -1,8 +1,15 @@
 def compare_and_add(num1, num2, num3):
     
-    bool = num1>num2
-    print (bool)
-    num = num1+num3
-    bool = num>num2
-    print (bool)
+    if num1 > num2:
+        print ("true")
+    else:
+        print ("false")
+        
+    num1 = num1 + num3
+    
+    if num1 > num2:
+        print ("true")
+    else:
+        print ("false")
+        
    
