07d43f56c8504262c195ccf47d488d03fe89b339
ParveendThakur <parveend1234@gmail.com>
2024-04-17 00:13:46 +0530

    DONE (Compare two Numbers)

---
+++ Compare two Numbers.py
@@ -0,0 +1,17 @@
+def compare_operations(num1, num2):
+    
+    if num1 > num2:
+        print ("true")
+    else :
+        print ("false")
+        
+    if num1 < num2:
+        print ("true")
+    else :
+        print ("false")
+        
+    if num1 == num2:
+        print ("true")
+    else :
+        print ("false")    
+    
