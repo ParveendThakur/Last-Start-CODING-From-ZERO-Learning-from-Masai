cab436b42abff91f1a04dd4462b513078eab112b
ParveendThakur <parveend1234@gmail.com>
2024-04-16 20:17:32 +0530

    Done Enough Fuel Consumption

---
+++ Enough Fuel Consumption.py
@@ -0,0 +1,8 @@
+def check_fuel_distance(fuel, distance):
+    required = fuel*distance
+    enough = 50
+    if enough < required:
+        print("Enough")
+    else: 
+        print ("Go On")   
+   
