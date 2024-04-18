01791c050265074cc6eaa866883e7f89101fee69
ParveendThakur <parveend1234@gmail.com>
2024-04-18 23:19:27 +0530

    Eligibility for Marriage

---
+++ Eligibility for Marriage.py
@@ -0,0 +1,10 @@
+# Instructions
+# Design a flowchart to determine whether an individual is eligible to get married based on their age and gender, considering legal requirements
+
+# Submission
+# Submitted Link:
+# https://vimeo.com/936367428?share=copy
+
+# Submitted on
+# 18 Apr, 2024 at 11:04 PM
+
