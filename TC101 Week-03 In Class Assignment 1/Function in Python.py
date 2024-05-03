3a072d9a6dce041ac3cfe947572f49c23ce68040
ParveendThakur <parveend1234@gmail.com>
2024-05-03 15:34:31 +0530

    Function in Python

--- TC101 Week-03 In Class Assignment 1/Function in Python.py
+++ TC101 Week-03 In Class Assignment 1/Function in Python.py
@@ -1,1 +1,21 @@
 # Python function is a block of code, that runs only when it is called. It is programmed to return the specific task. You can pass values in functions called parameters.
+# Python Functions
+# A function is a block of code which only runs when it is called.
+
+# You can pass data, known as parameters, into a function.
+
+# A function can return data as a result.
+
+# Creating a Function
+# In Python a function is defined using the def keyword:
+
+
+
+def hello():
+  print("Hello World")
+
+
+hello()
+
+
+hello()
