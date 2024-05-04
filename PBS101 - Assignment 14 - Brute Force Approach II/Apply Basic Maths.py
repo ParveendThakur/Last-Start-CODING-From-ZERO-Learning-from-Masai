40e9826685ae207cfa807a4cecfe1f0a1d1c2a3c
ParveendThakur <parveend1234@gmail.com>
2024-05-05 00:56:39 +0530

    Partial Accepted

--- PBS101 - Assignment 14 - Brute Force Approach II/Apply Basic Maths.py
+++ PBS101 - Assignment 14 - Brute Force Approach II/Apply Basic Maths.py
@@ -1,2 +1,16 @@
 # https://oj.masaischool.com/contest/11912?password=240203
 
+def solve(N,A):
+    
+    #def find_index_to_remove(N, A):
+    total_sum = sum(A)
+    for i in range(N):
+        if (total_sum - A[i]) % 7 == 0:
+            print([i])
+    print(-1)
+
+
+
+
+
+  
