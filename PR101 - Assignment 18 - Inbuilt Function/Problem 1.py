8a763d4886c08a57edb13dc8084d02918047f977
ParveendThakur <parveend1234@gmail.com>
2024-05-05 01:10:39 +0530

    Problem 1: Custom join Function

--- PR101 - Assignment 18 - Inbuilt Function/Problem 1.py
+++ PR101 - Assignment 18 - Inbuilt Function/Problem 1.py
@@ -1,3 +1,23 @@
 # https://students.masaischool.com/assignments/38738/problems/33271/134566
 # Questions Link : Click here
 # https://masai-school.notion.site/Inbuilt-Functions-eb495df093a94ff7847d0632bf5cfc31
+
+def custom_join(strings, separator):
+    # Check if the input list is empty
+    if not strings:
+        return ''
+    
+    # Initialize an empty string to store the result
+    result = strings[0]
+    
+    # Concatenate each element of the list with the separator
+    for string in strings[1:]:
+        result += separator + string
+    
+    return result
+
+# Test the function
+input_list = ['Hello', 'World']
+separator = ' '
+output = custom_join(input_list, separator)
+print(output)  # Output: 'Hello World'
