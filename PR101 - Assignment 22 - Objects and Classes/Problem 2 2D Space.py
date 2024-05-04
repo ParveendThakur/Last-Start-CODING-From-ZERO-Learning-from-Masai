e275585c7572a9a1b4f4bdc10968c088fd665315
ParveendThakur <parveend1234@gmail.com>
2024-05-04 20:47:32 +0530

    Problem 2 : 2D Space

---
+++ PR101 - Assignment 22 - Objects and Classes/Problem 2 2D Space.py
@@ -0,0 +1,36 @@
+import math
+
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def get_x(self):
+        return self.x
+
+    def get_y(self):
+        return self.y
+
+    def set_x(self, x):
+        self.x = x
+
+    def set_y(self, y):
+        self.y = y
+
+    def distance(self, x=None, y=None):
+        if x is None and y is None:
+            return math.sqrt(self.x ** 2 + self.y ** 2)
+        elif isinstance(x, Point):
+            return math.sqrt((x.get_x() - self.x) ** 2 + (x.get_y() - self.y) ** 2)
+        else:
+            return math.sqrt((x - self.x) ** 2 + (y - self.y) ** 2)
+
+# TEST EXAMPLE
+
+first = Point(6, 5)
+second = Point(3, 1)
+print("distance(0,0)= ", first.distance())
+print("distance(second)= ", first.distance(second))
+print("distance(2,2)= ", first.distance(2, 2))
+point = Point()
+print("distance()= ", point.distance())
