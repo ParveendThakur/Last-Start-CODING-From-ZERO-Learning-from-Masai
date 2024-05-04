bc1200e0fb435fc85482f278dae1784f51db2f74
ParveendThakur <parveend1234@gmail.com>
2024-05-05 02:06:18 +0530

    done(Map Characters)

--- PR101  Assignment - 20 Key-Value Pair [Level 1]/Map Characters.py
+++ PR101  Assignment - 20 Key-Value Pair [Level 1]/Map Characters.py
@@ -1,1 +1,12 @@
 # https://oj.masaischool.com/contest/11899?password=masai433
+
+def solve(N):
+    
+    if 1 <= N <= 26:
+        for i in range(N):
+            alphabet = chr(ord('a') + i)  # Convert index to corresponding lowercase alphabet
+            print(alphabet + '-' + str(i + 1))
+    else:
+        print("Invalid input. N should be between 1 and 26.")
+
+  
