056bbcc1fb34e0bffb1238568c540b8fc0643281
ParveendThakur <parveend1234@gmail.com>
2024-05-05 01:19:16 +0530

    Problem 5: Custom contains Function

---
+++ PR101 - Assignment 18 - Inbuilt Function/Problem 5.py
@@ -0,0 +1,8 @@
+def custom_contains(lst, element):
+    return element in lst
+
+# Test the function
+input_list = [1, 2, 3, 4, 5]
+search_element = 3
+output = custom_contains(input_list, search_element)
+print(output)  # Output: True
