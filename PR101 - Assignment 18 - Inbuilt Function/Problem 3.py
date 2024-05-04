33d5bc38fc54496a23400f5e7ac811544988c4b1
ParveendThakur <parveend1234@gmail.com>
2024-05-05 01:16:15 +0530

    Problem 3: Custom slice Function

--- PR101 - Assignment 18 - Inbuilt Function/Problem 3.py
+++ PR101 - Assignment 18 - Inbuilt Function/Problem 3.py
@@ -1,2 +1,18 @@
 # https://students.masaischool.com/assignments/38738/problems/33271/134566
 
+def custom_slice(lst, start, end):
+    # Handle negative indices
+    if start < 0:
+        start += len(lst)
+    if end < 0:
+        end += len(lst)
+    
+    # Slice the list using the start and end indices
+    return lst[start:end]
+
+# Test the function
+input_list = [1, 2, 3, 4, 5]
+start_index = 1
+end_index = 4
+output = custom_slice(input_list, start_index, end_index)
+print(output)  # Output: [2, 3, 4]
