2ca21e3bfe0b110c5ed8576586c9750b255215ce
ParveendThakur <parveend1234@gmail.com>
2024-05-05 00:58:01 +0530

    Partial Accepted

--- PBS101 - Assignment 14 - Brute Force Approach II/Sum of Special Pairs.py
+++ PBS101 - Assignment 14 - Brute Force Approach II/Sum of Special Pairs.py
@@ -1,3 +1,12 @@
 # https://oj.masaischool.com/contest/11912?password=240203
 
+def solve(N,A):
+    
+    total_sum = 0
+    for i in range(N):
+        for j in range(i + 1, N):
+            if (j - i):
+                total_sum += abs(A[j] - A[i])
+    print(total_sum)
 
+  
