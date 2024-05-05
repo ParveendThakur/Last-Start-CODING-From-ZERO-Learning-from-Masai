5f8b516028e37edccef8f1d125ffb7e9421001ea
ParveendThakur <parveend1234@gmail.com>
2024-05-05 23:54:40 +0530

    Done

--- PR101  Assignment - 19 (Functions) [Level 2]/Temperature Conversion.py
+++ PR101  Assignment - 19 (Functions) [Level 2]/Temperature Conversion.py
@@ -1,2 +1,18 @@
 # https://students.masaischool.com/assignments/38650/problems/31771/134453
 
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5/9
+    return celsius
+
+# Example usage
+celsius_temperature = 25
+fahrenheit_temperature = celsius_to_fahrenheit(celsius_temperature)
+print(f"{celsius_temperature} degrees Celsius is equal to {fahrenheit_temperature} degrees Fahrenheit.")
+
+fahrenheit_temperature = 77
+celsius_temperature = fahrenheit_to_celsius(fahrenheit_temperature)
+print(f"{fahrenheit_temperature} degrees Fahrenheit is equal to {celsius_temperature} degrees Celsius.")
