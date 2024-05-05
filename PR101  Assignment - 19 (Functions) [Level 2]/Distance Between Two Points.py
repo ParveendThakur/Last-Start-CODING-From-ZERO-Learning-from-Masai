2ba63ea896f5cd81dcd64815aa6a13a618ce8d6b
ParveendThakur <parveend1234@gmail.com>
2024-05-05 23:50:52 +0530

    Distance Between Two Points Done

--- PR101  Assignment - 19 (Functions) [Level 2]/Distance Between Two Points.py
+++ PR101  Assignment - 19 (Functions) [Level 2]/Distance Between Two Points.py
@@ -1,3 +1,20 @@
 # https://students.masaischool.com/assignments/38650?tab=assignmentDetails
 # https://drive.google.com/drive/folders/17ksLOE4BMf2-oH7JR4FY8n8V63kvCxaZ?usp=sharing
 # https://students.masaischool.com/assignments/38650/problems/31770/134452
+
+import math
+
+def distance(x1, y1, x2, y2):
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+# Sample Input
+point1 = (2, 3)
+point2 = (5, 7)
+
+# Unpack the coordinates
+x1, y1 = point1
+x2, y2 = point2
+
+# Calculate distance
+result = distance(x1, y1, x2, y2)
+print(result)  # Output: 5.0
