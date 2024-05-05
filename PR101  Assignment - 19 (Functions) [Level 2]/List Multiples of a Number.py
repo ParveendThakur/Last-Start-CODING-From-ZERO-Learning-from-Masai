013d5f6634fcc2835a052f56ad78d5104674dbc7
ParveendThakur <parveend1234@gmail.com>
2024-05-06 00:02:44 +0530

    done

--- PR101  Assignment - 19 (Functions) [Level 2]/List Multiples of a Number.py
+++ PR101  Assignment - 19 (Functions) [Level 2]/List Multiples of a Number.py
@@ -1,2 +1,9 @@
 # https://students.masaischool.com/assignments/38650/problems/31774/134455
 
+def listMultiples(n, limit):
+
+    for i in range(1, n + 1):
+        print(i * limit, end=", ")
+
+# Example usage
+listMultiples(3, 5)  # Output: 5, 10, 15,
