6386318f057fd8e002cf2d2b19f527714207a99e
ParveendThakur <parveend1234@gmail.com>
2024-05-05 19:03:56 +0530

    done

---
+++ PR101  Assignment - 21 Key-Value Pair [Level 2]/Pasha and Shasha and valid anagram.py
@@ -0,0 +1,14 @@
+def solve(str1, str2):
+    
+    
+    
+    # Sort the strings
+    sorted_s1 = sorted(str1)
+    sorted_s2 = sorted(str2)
+    
+    # Check if the sorted strings are equal
+    if sorted_s1 == sorted_s2:
+        print("True")
+    else:
+        print("False")
+
