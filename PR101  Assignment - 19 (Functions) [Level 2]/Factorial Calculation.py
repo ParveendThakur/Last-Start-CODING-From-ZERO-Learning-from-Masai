257df587abc5c50b4c0ef44d31a1b5437752abcb
ParveendThakur <parveend1234@gmail.com>
2024-05-06 00:05:38 +0530

    done

--- PR101  Assignment - 19 (Functions) [Level 2]/Factorial Calculation.py
+++ PR101  Assignment - 19 (Functions) [Level 2]/Factorial Calculation.py
@@ -1,2 +1,12 @@
 # https://students.masaischool.com/assignments/38650/problems/31775/134456
 
+def calculateFactorial(n):
+    
+    factorial = 1
+    for i in range(1, n + 1):
+        factorial *= i
+    return factorial
+
+# Example usage
+result = calculateFactorial(5)
+print("Result:", result)  # Output: 120
