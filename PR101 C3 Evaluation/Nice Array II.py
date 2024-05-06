9d1c58694a74bce1dd4478f451bc1a2965885d4f
ParveendThakur <parveend1234@gmail.com>
2024-05-06 23:56:23 +0530

    done

--- PR101 C3 Evaluation/Nice Array II.py
+++ PR101 C3 Evaluation/Nice Array II.py
@@ -1,3 +1,4 @@
 # https://students.masaischool.com/assignments/38812?tab=assignmentDetails
 # https://oj.masaischool.com/contest/11917?password=evaluation3
 # https://oj.masaischool.com/contest/11917/problems
+# https://oj.masaischool.com/contest/11917/problem/01
