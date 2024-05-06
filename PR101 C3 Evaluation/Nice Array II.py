b0975133e96de5639a03b21451ec18113e2a668d
ParveendThakur <parveend1234@gmail.com>
2024-05-06 23:58:17 +0530

    done

--- PR101 C3 Evaluation/Nice Array II.py
+++ PR101 C3 Evaluation/Nice Array II.py
@@ -2,3 +2,14 @@
 # https://oj.masaischool.com/contest/11917?password=evaluation3
 # https://oj.masaischool.com/contest/11917/problems
 # https://oj.masaischool.com/contest/11917/problem/01
+
+def solve(N,array,K):
+    
+    odd = sum(1 for num in array if num % 2 != 0)
+    
+    if odd > K:
+        print("Nice Array")
+    else:
+        print("Bad Array")
+        
+    
