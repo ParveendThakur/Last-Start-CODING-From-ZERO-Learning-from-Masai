04ffe688b20eb6362fc85a46e9c59730551f436a
ParveendThakur <parveend1234@gmail.com>
2024-05-08 00:49:32 +0530

    Q-1

--- PBS101 - Assignment 15 - Multidimensional Arrays I/Traverse a 2D Array.py
+++ PBS101 - Assignment 15 - Multidimensional Arrays I/Traverse a 2D Array.py
@@ -1,2 +1,3 @@
 # https://students.masaischool.com/assignments/38857?tab=assignmentDetails
 # https://oj.masaischool.com/contest/11930/problems
+# https://oj.masaischool.com/contest/11930/problem/01
