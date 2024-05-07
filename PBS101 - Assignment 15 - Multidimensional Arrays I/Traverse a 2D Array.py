093d0fad085b394eacbe58dc41f4a98cd70c60a4
ParveendThakur <parveend1234@gmail.com>
2024-05-08 01:00:45 +0530

    not done

--- PBS101 - Assignment 15 - Multidimensional Arrays I/Traverse a 2D Array.py
+++ PBS101 - Assignment 15 - Multidimensional Arrays I/Traverse a 2D Array.py
@@ -1,3 +1,20 @@
 # https://students.masaischool.com/assignments/38857?tab=assignmentDetails
 # https://oj.masaischool.com/contest/11930/problems
 # https://oj.masaischool.com/contest/11930/problem/01
+
+def solve(N,M,arr):
+    
+    # Input
+#N, M = map(int, input().split())
+#matrix = [list(map(int, input().split())) for _ in range(N)]
+
+# Traversing and printing in the zigzag pattern
+    for i in range(N):
+        if i % 2 == 0:
+            for j in range(M):
+                print(arr[i][j], end=' ')
+        else:
+            for j in range(M - 1, -1, -1):
+                print(arr[i][j], end=' ')
+
+    
