370f2804c40e365645fc4b33d655e6d07afde53f
ParveendThakur <parveend1234@gmail.com>
2024-05-08 15:58:12 +0530

    problem/01

--- PBS101 - Assignment 16 - Multidimensional Arrays II/Even Sum in Columns.py
+++ PBS101 - Assignment 16 - Multidimensional Arrays II/Even Sum in Columns.py
@@ -1,2 +1,3 @@
 # https://students.masaischool.com/assignments/38864?tab=assignmentDetails
 # https://oj.masaischool.com/contest/11933/problems
+# https://oj.masaischool.com/contest/11933/problem/01
