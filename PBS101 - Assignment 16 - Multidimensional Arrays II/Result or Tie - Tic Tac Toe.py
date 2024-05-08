b5a2d043961e943a2cc6fa4265fa9c52e8e4728c
ParveendThakur <parveend1234@gmail.com>
2024-05-09 00:06:25 +0530

    done

--- PBS101 - Assignment 16 - Multidimensional Arrays II/Result or Tie - Tic Tac Toe.py
+++ PBS101 - Assignment 16 - Multidimensional Arrays II/Result or Tie - Tic Tac Toe.py
@@ -1,1 +1,26 @@
 # https://oj.masaischool.com/contest/11933/problems
+
+def solve(board):
+    
+    
+
+    # Check rows and columns for a win
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] != '0':
+            print(board[i][0])
+            exit()
+        if board[0][i] == board[1][i] == board[2][i] != '0':
+            print(board[0][i])
+            exit()
+
+    # Check diagonals for a win
+    if board[0][0] == board[1][1] == board[2][2] != '0':
+        print(board[0][0])
+        exit()
+    if board[0][2] == board[1][1] == board[2][0] != '0':
+        print(board[0][2])
+        exit()
+
+    print("Tie")
+
+    
