020f688bccaf28a476c9a697eebff78a5b137974
ParveendThakur <parveend1234@gmail.com>
2024-05-10 23:49:45 +0530

    House Making --Done

--- PR101 Unit Evaluation/House Making.py
+++ PR101 Unit Evaluation/House Making.py
@@ -1,3 +1,12 @@
 # https://students.masaischool.com/assignments/38959?tab=assignmentDetails
 # https://oj.masaischool.com/contest/11949/problems
 # https://oj.masaischool.com/contest/11949/problem/01
+
+def solve(N,K):
+    
+    b = N*K
+    if b >= 1000 and b % 5 ==0:
+        print("yes")
+    else:
+        print("no")
+  
