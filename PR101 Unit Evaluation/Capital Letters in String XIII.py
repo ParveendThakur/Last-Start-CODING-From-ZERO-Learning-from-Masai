fc91d9e3e0f50eda415397861874e7d8f0eef1f9
ParveendThakur <parveend1234@gmail.com>
2024-05-11 23:42:16 +0530

    Done_ Capital Letters in String XIII

--- PR101 Unit Evaluation/Capital Letters in String XIII.py
+++ PR101 Unit Evaluation/Capital Letters in String XIII.py
@@ -1,1 +1,10 @@
 # https://oj.masaischool.com/contest/11949/problem/05
+
+def solve(N,string):
+    
+    count = sum(1 for char in string if char.isupper())
+    
+    r = 13*count
+    
+    print(r)
+  
