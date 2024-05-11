73b21e0f5be96a60c9096a7a36a90d8d9d1c13fc
ParveendThakur <parveend1234@gmail.com>
2024-05-12 00:08:27 +0530

    Not Done

--- PR101 Unit Evaluation/2D Array - Weird Traversal I.py
+++ PR101 Unit Evaluation/2D Array - Weird Traversal I.py
@@ -1,1 +1,15 @@
 # https://oj.masaischool.com/contest/11949/problem/09
+
+def solve(N,arr):
+        
+    c1 = sum(arr[i][0] for i in range (N // 2))
+    
+    c2 = sum(arr[N // 2])
+    
+    c3 = sum (arr[i][N-1] for i in range (N // 2 , N))
+    
+    out = 3*c1 + c2 + 2*c3
+    
+    print(out)
+    
+    
