d1c8a525729aff1801e63cb8354e1fad4844d6b9
ParveendThakur <parveend1234@gmail.com>
2024-05-11 23:38:21 +0530

    Done -- Array Half Traversals X

--- PR101 Unit Evaluation/Array Half Traversals X.py
+++ PR101 Unit Evaluation/Array Half Traversals X.py
@@ -1,1 +1,14 @@
 # https://oj.masaischool.com/contest/11949/problem/02
+
+def solve(N, arr):
+    
+    mid = N // 2
+    
+    sum_f_h = sum(arr[:mid])
+    sum_s_h = sum(arr[mid:])
+    
+    r = 7*sum_f_h + 2 * sum_s_h
+    
+    print(r)
+    
+    
