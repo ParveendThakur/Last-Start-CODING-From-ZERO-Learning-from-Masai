35118388f8be4aa93f77f6f4154095fbf3e30911
ParveendThakur <parveend1234@gmail.com>
2024-05-11 23:40:59 +0530

    Done -- String Power Calculator X

--- PR101 Unit Evaluation/String Power Calculator X.py
+++ PR101 Unit Evaluation/String Power Calculator X.py
@@ -1,1 +1,11 @@
 # https://oj.masaischool.com/contest/11949/problem/04
+
+def solve(N,string):
+    
+    v = sum(1 for char in string if char.lower() in 'aeiou')
+    c = N-v
+    
+    p = 7*v+2*c
+    
+    print(p)
+  
