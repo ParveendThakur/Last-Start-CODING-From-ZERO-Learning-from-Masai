86037f91739da7781af51453d29a52ea53bfe69c
ParveendThakur <parveend1234@gmail.com>
2024-05-11 23:39:41 +0530

    Done --Validate

--- PR101 Unit Evaluation/Validate Team Strength.py
+++ PR101 Unit Evaluation/Validate Team Strength.py
@@ -1,1 +1,14 @@
 # https://oj.masaischool.com/contest/11949/problem/03
+
+def solve(n,k,arr):
+    
+    a = sum(arr[:n])
+    b = sum (arr[n:])
+    
+    d = abs(a - b)
+    
+    if d <= k:
+        print("Valid")
+    else:
+        print("Invalid")
+    
