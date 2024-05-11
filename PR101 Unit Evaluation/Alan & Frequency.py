d7073ed4d33fe2e78827b2355c0447a32c153947
ParveendThakur <parveend1234@gmail.com>
2024-05-11 23:43:32 +0530

    Done Alan & Frequency

--- PR101 Unit Evaluation/Alan & Frequency.py
+++ PR101 Unit Evaluation/Alan & Frequency.py
@@ -1,1 +1,15 @@
 # https://oj.masaischool.com/contest/11949/problem/06
+
+def solve(N,string):
+    
+    f = {}
+    
+    for char in string:
+        if char in f:
+            f[char] += 1
+        else:
+            f[char] =1
+            
+    for char in sorted(f):
+        print(char + "-" + str(f[char]))
+  
