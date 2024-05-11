15b900f1c9e41ddc010e4c970512919a337a2fae
ParveendThakur <parveend1234@gmail.com>
2024-05-12 00:07:28 +0530

    Done --- Matrix and Vowels

--- PR101 Unit Evaluation/Matrix and Vowels.py
+++ PR101 Unit Evaluation/Matrix and Vowels.py
@@ -1,1 +1,15 @@
 # https://oj.masaischool.com/contest/11949/problem/08
+
+def solve(N,M,mat):
+    
+    vowels = {'a', 'e', 'i', 'o', 'u'}
+    
+    for col in range (M):
+        vp = any(mat[row][col] in vowels 
+        for row in range(N))
+        if vp:
+            print ("Yes")
+        else:
+            print ("No")
+    
+
