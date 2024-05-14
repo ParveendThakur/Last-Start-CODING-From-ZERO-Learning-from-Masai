6f5a06ed7acae611b113bcce2fe335da738a7f99
ParveendThakur <parveend1234@gmail.com>
2024-05-14 23:46:46 +0530

    Detect Palindrome

--- Timed Contest-01/Detect Palindrome.py
+++ Timed Contest-01/Detect Palindrome.py
@@ -1,2 +1,3 @@
 # https://students.masaischool.com/assignments/39086?tab=assignmentDetails
 # https://oj.masaischool.com/contest/11994/problems
+# https://oj.masaischool.com/contest/11994/problem/01
