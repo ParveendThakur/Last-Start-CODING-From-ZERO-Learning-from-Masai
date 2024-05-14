e51d7c5023a177ffc8862d12f5016335db4df673
ParveendThakur <parveend1234@gmail.com>
2024-05-14 23:48:18 +0530

    not done --  Wrong Answer

--- Timed Contest-01/Product of Columns.py
+++ Timed Contest-01/Product of Columns.py
@@ -1,1 +1,14 @@
 # https://oj.masaischool.com/contest/11994/problem/02
+
+# name = input()                  # Reading input from STDIN
+# print('Hi, %s.' % name)         # Writing output to STDOUT
+
+N = 3
+M = 2
+arr = [[1,2], [3,4], [5,6]]
+
+for j in range (M):
+    product = 1
+    for i in range (N):
+        product *= arr[i][j]
+    print(product)    
