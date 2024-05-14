b0fbd125bbd82b229a104fd02015fbbf4a006f20
ParveendThakur <parveend1234@gmail.com>
2024-05-14 23:47:10 +0530

    Done -- Detect Palindrome

--- Timed Contest-01/Detect Palindrome.py
+++ Timed Contest-01/Detect Palindrome.py
@@ -1,3 +1,12 @@
 # https://students.masaischool.com/assignments/39086?tab=assignmentDetails
 # https://oj.masaischool.com/contest/11994/problems
 # https://oj.masaischool.com/contest/11994/problem/01
+
+# name = input()                  # Reading input from STDIN
+# print('Hi, %s.' % name)         # Writing output to STDOUT
+num = int(input())
+ns = str(num)
+if ns == ns[::-1]:
+    print("Yes")
+else:
+    print("No")
