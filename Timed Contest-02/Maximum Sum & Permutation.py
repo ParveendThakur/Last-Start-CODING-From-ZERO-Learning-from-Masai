7c3a8a6ff9385fe946535070d9cc227edb996cb2
ParveendThakur <parveend1234@gmail.com>
2024-05-17 00:34:35 +0530

    Accepted --- Maximum Sum & Permutation

--- Timed Contest-02/Maximum Sum & Permutation.py
+++ Timed Contest-02/Maximum Sum & Permutation.py
@@ -1,1 +1,14 @@
 # https://oj.masaischool.com/contest/12027/problem/02
+
+#//Enter code here
+
+def max_sum_permutation(n,arr):
+    arr.sort()
+    max_sum = 0
+    for i in range (n):
+        max_sum += arr[i]*i
+    return max_sum
+    
+n = int(input())
+arr = list(map(int,input().split()))
+print(max_sum_permutation(n,arr))
