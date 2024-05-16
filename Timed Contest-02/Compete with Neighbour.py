61bec7c7639da0d95b66e4cac6e76859431f1db5
ParveendThakur <parveend1234@gmail.com>
2024-05-16 23:40:24 +0530

    ( Partial Accepted) -- Compete with Neighbour

--- Timed Contest-02/Compete with Neighbour.py
+++ Timed Contest-02/Compete with Neighbour.py
@@ -1,3 +1,16 @@
 # https://students.masaischool.com/assignments/39270?tab=assignmentDetails
 # https://oj.masaischool.com/contest/12027/problems
 # https://oj.masaischool.com/contest/12027/problem/01
+
+#//Enter code here
+
+def count_larger_then_neighbord(arr):
+    count = 0
+    for i in range (1 , len (arr) -1):
+        if arr [i]>arr[i-1] and arr [i]>arr[i+1]:
+            count +=1
+    return count
+    
+n = int(input())
+arr = list(map(int,input().split()))
+print(count_larger_then_neighbord(arr))
