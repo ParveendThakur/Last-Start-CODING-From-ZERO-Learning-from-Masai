17eeb04d537f3af1900c70fdd5335f1ed4d5ab4a
ParveendThakur <parveend1234@gmail.com>
2024-05-16 23:39:56 +0530

    problem/01

--- Timed Contest-02/Compete with Neighbour.py
+++ Timed Contest-02/Compete with Neighbour.py
@@ -1,2 +1,3 @@
 # https://students.masaischool.com/assignments/39270?tab=assignmentDetails
 # https://oj.masaischool.com/contest/12027/problems
+# https://oj.masaischool.com/contest/12027/problem/01
