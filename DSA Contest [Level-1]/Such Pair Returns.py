31f2cb580679f0758dde8782639709ec06890b13
ParveendThakur <parveend1234@gmail.com>
2024-05-18 23:46:02 +0530

    Done --- Such Pair Returns

--- DSA Contest [Level-1]/Such Pair Returns.py
+++ DSA Contest [Level-1]/Such Pair Returns.py
@@ -1,3 +1,23 @@
 # https://students.masaischool.com/assignments/39250?tab=assignmentDetails
 # https://oj.masaischool.com/contest/12017/problems
 # https://oj.masaischool.com/contest/12017/problem/1
+
+#//Enter code here
+
+def such_pair_returns(N, K, integers):
+    seen = set()
+    for num in integers:
+        complement = K - num
+        if complement in seen:
+            return 1
+        seen.add(num)
+    return -1
+
+# Input processing
+T = int(input())
+for _ in range(T):
+    N, K = map(int, input().split())
+    integers = list(map(int, input().split()))
+    
+    result = such_pair_returns(N, K, integers)
+    print(result)
