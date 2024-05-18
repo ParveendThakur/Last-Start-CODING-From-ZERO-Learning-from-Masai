c7c1277d484e0d94db678b9cddba8f23515c305c
ParveendThakur <parveend1234@gmail.com>
2024-05-19 00:24:04 +0530

    Accepted-- Reverse Array 12

--- DSA Contest [Level-1]/Reverse Array 12.py
+++ DSA Contest [Level-1]/Reverse Array 12.py
@@ -1,1 +1,11 @@
 # https://oj.masaischool.com/contest/12017/problem/6
+
+#//Enter code here
+
+# Reading input
+n = int(input().strip())
+arr = list(map(int, input().split()))
+
+# Reversing the array using slicing and printing the result
+reversed_arr = arr[::-1]
+print(*reversed_arr)
