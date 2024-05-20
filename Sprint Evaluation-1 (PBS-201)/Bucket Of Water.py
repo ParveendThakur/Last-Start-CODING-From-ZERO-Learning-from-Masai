c79430c625ea28f81c58c357b97a7e5e58f2442d
ParveendThakur <parveend1234@gmail.com>
2024-05-20 23:06:43 +0530

    Bucket Of Water

--- Sprint Evaluation-1 (PBS-201)/Bucket Of Water.py
+++ Sprint Evaluation-1 (PBS-201)/Bucket Of Water.py
@@ -1,2 +1,3 @@
 # https://students.masaischool.com/assignments/39404?tab=assignmentDetails
 # https://oj.masaischool.com/contest/12023/problems
+# https://oj.masaischool.com/contest/12023/problem/101-100
