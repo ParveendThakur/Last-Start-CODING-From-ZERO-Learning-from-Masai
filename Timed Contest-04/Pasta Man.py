4b9bc8a89f1b05c7ad0db591e8c871aa2558d8d1
ParveendThakur <parveend1234@gmail.com>
2024-05-25 23:12:16 +0530

    problem/01

--- Timed Contest-04/Pasta Man.py
+++ Timed Contest-04/Pasta Man.py
@@ -1,2 +1,3 @@
 # https://students.masaischool.com/assignments/39880?tab=assignmentDetails
 # https://oj.masaischool.com/contest/12100/problems
+# https://oj.masaischool.com/contest/12100/problem/01
