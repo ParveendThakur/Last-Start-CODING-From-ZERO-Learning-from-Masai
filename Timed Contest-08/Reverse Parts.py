c48682270e8e08728691b66b5f704bf599c78736
ParveendThakur <parveend1234@gmail.com>
2024-06-06 18:02:23 +0530

    problem/01

--- Timed Contest-08/Reverse Parts.py
+++ Timed Contest-08/Reverse Parts.py
@@ -1,2 +1,3 @@
 # https://students.masaischool.com/assignments/40541?tab=assignmentDetails
 # https://oj.masaischool.com/contest/12257/problems
+# https://oj.masaischool.com/contest/12257/problem/01
