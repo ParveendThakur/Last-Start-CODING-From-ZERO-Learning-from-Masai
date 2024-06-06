dc8b1f09f0847470e44c0b81db686b8e555f647f
ParveendThakur <parveend1234@gmail.com>
2024-06-06 18:05:11 +0530

    Accepted -- Reverse Parts

--- Timed Contest-08/Reverse Parts.py
+++ Timed Contest-08/Reverse Parts.py
@@ -1,3 +1,25 @@
 # https://students.masaischool.com/assignments/40541?tab=assignmentDetails
 # https://oj.masaischool.com/contest/12257/problems
 # https://oj.masaischool.com/contest/12257/problem/01
+
+#//Enter code here
+
+def rp(N, s, L, R):
+    
+    substring = s[L:R+1]
+    
+   
+    rs = substring[::-1]
+    
+   
+    updated_string = s[:L] + rs + s[R+1:]
+    
+    return updated_string
+
+# Input
+N = int(input())
+s = input().strip()
+L, R = map(int, input().split())
+
+# Output
+print(rp(N, s, L, R))
