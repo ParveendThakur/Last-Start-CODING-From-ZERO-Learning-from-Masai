c08d48b0660c2cb4c0c94cdb2568c8532fcd60ad
ParveendThakur <parveend1234@gmail.com>
2024-06-20 22:54:45 +0530

    problem/01

--- DSA Timed Contest - 01/Detect Palindrome.py
+++ DSA Timed Contest - 01/Detect Palindrome.py
@@ -1,2 +1,3 @@
 # https://students.masaischool.com/assignments/40884?tab=assignmentDetails
 # https://oj.masaischool.com/contest/12364/problems
+# https://oj.masaischool.com/contest/12364/problem/01
